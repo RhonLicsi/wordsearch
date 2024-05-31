a6280484282cc0519f86d35ed6b9da437516fa4b
rhon-licsi <rhoncedricklicsi@gmail.com>
2024-05-31 14:45:13 +0800

    Update May 31, 2024

--- word-search.lisp
+++ word-search.lisp
@@ -1,3 +1,14 @@
 (in-package :word-search)
 
-(defun searcher ())
+(defparameter *grid* #2A(
+                         (#\W #\E #\L #\C #\O #\M #\E)
+                         (#\T #\O #\T #\H #\E #\G #\A)
+                         (#\W #\O #\R #\D #\S #\E #\A)
+                         (#\R #\C #\H #\G #\A #\M #\E)
+                         (#\E #\X #\A #\M #\P #\L #\E)
+                         ))
+
+(defparameter *words* '("WELCOME" "TO" "THE" "WORD" "SEARCH" "GAME" "EXAMPLE"))
+
+(defun foo ()
+  (format t "hello"))
