97cabae9fba260a5d34892ab6a0a8e5d178fb3d5
rhon-licsi <rhoncedricklicsi@gmail.com>
2024-06-01 01:41:28 +0800

    June 1, 2024

--- word-search.lisp
+++ word-search.lisp
@@ -11,5 +11,16 @@
       (format t "~a "(random-letter)))
     (format t "~%")))
 
+CC
+(defvar word-set '("APPLE" "BANANA" "ORANGE" "GRAPE" "PINEAPPLE"))
+
+(defun word-space (word)
+  (loop for letter across word do (format t "~a " letter))
+  (format t "~%"))
+
+(defun print-word (words)
+  (loop for word in words
+        do (word-space word)))
+
 
 
